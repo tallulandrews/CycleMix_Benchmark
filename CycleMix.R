fd553d2802018550dd74d4ecaac470903745c766
Tallulah Andrews <tallulandrews@gmail.com>
2026-04-25 15:45:03 -0400

    Add new versions of CycleMix

--- CycleMix.R
+++ CycleMix.R
@@ -16,6 +16,32 @@ run_CycleMix <- function(counts, gene_table) {
 	return(list(phase=out$phase, time=elapsed.time))
 }
 
+run_CycleMix_matrix <- function(norm, gene_table) {
+	print("Cyclemix is classifying cells")
+	elapsed.time <- system.time(out <- classifyCells(norm, gene_table, do.scale=FALSE))[3]
+	print(paste("Elapsed time:", elapsed.time))
+	return(list(phase=out$phase, time=elapsed.time))
+}
+
+run_CycleMix_knnSmooth <- function(seur_obj, gene_table, cluster_col=NULL) {
+	counts <- GetAssayData(seur_obj, layer="counts", assay="RNA")
+	SCE <- SingleCellExperiment(assays=list(counts=counts), rowData=data.frame(feature_symbol=rownames(counts)))
+	logcounts(SCE) <- normalizeCounts(SCE, log=TRUE, pseudo_count=1, size.factors=colSums(counts)/median(colSums(counts)))
+	print("Cyclemix is classifying cells")
+	#start.time <- Sys.time()
+	elapsed.time1 <- system.time(out <- classifyCells(SCE, gene_table))[3]
+
+	if (is.null(cluster_col)) {
+		elapsed.time2 <- system.time(out <- knnSmooth(out, dims=seur_obj@reductions$pca, k=20))[3]
+	} else {
+		elapsed.time2 <- system.time(out <- knnSmooth(out, dims=seur_obj@reductions$pca, k=20, clusters=seur_obj@meta.data[,cluster_col]))[3]
+	}
+	#end.time <- Sys.time()
+	#elapsed.time=end.time - start.time
+	print(paste("Elapsed time:", elapsed.time))
+	return(list(phase=out$phase, time=elapsed.time1+elapsed.time2))
+}
+
 run_CycleMix_scores <- function(counts, gene_table) {
         SCE <- SingleCellExperiment(assays=list(counts=counts), rowData=data.frame(feature_symbol=rownames(counts)))
         logcounts(SCE) <- normalizeCounts(SCE, log=TRUE, pseudo_count=1, size.factors=colSums(counts)/median(colSums(counts)))
